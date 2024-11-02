906f81f4445dee4a2338b36cff5f03accf4e74a0
RAHUL-AMBARAGONDA <rahulambaragonda@gmail.com>
2024-11-01 21:10:58 -0400

    file7

---
+++ file 7.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+userid = $ {id -u}
+if [$userid -ne 0]
+then 
+echo "error : plz run the script through root access"
+fi 
+yum install maven -
+
+if [$? -ne 0 ]
+then 
+echo "INSTALLION FAILURE "
+else 
+echo "INSTALLION SUCCESS "
+fi
