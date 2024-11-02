0adab66e1f935275f72284af04ab2c3aac177a14
RAHUL-AMBARAGONDA <rahulambaragonda@gmail.com>
2024-11-01 21:28:01 -0400

    file8

--- file8.sh
+++ file8.sh
@@ -1,11 +1,13 @@
 #!bin/bash
 
 USERID= $(id - u)
+
 if [ $USERID -ne 0 ]
 then 
 echo "error : plz run the script through root access"
+
 fi 
-yum install maven -
+yum install maven -Y
 
 if [ $? -ne 0 ]
 then 
