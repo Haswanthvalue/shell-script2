b5fb5c4f15fca0052aa00694993a1c8ae224c7d0
RAHUL-AMBARAGONDA <rahulambaragonda@gmail.com>
2024-11-01 20:43:37 -0400

    file 3

--- file4.sh
+++ file4.sh
@@ -1,8 +1,8 @@
 !bin/bash
 
-echo person 1 : ram
-echo person 2: vinay
-echo $person 1:hii
-echo $person 2:hii how are you/
-echo $person 1:i am good 
+echo person 1 : $1
+echo person 2: $2
+echo $person1:hii
+echo $person2:hii how are you/
+echo $person1:i am good 
 
