15d78c19eba66de94bc07b3518bf61344bd62207
RAHUL-AMBARAGONDA <rahulambaragonda@gmail.com>
2024-11-01 20:54:22 -0400

    file5

--- file5.sh
+++ file5.sh
@@ -1,7 +1,11 @@
 #!bin/bash 
 
 echo "enter username "
-read USERNAME
+read -s USERNAME
 echo "user name entered is : $USERNAME"
+echo "enter password"
+read -s PASSWORD
+echo "password entered is : $PASSWORD"
+
 
 
